895922fe8aaef8d34ea2ff4b99f7aeff126276ad
Mohamed <mohamedkoty60@gmail.com>
2024-10-02 14:17:34 +0300

    My First PYthon Code

---
+++ MYPythonLanguage.py
@@ -0,0 +1,9 @@
+def square(num):
+    return num * num
+
+# Example usage
+number = int(input("Enter a number: "))
+result = square(number)
+
+# Output the result
+print(f"The square of {number} is {result}")
